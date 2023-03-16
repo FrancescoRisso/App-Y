64acc3e12ebe497e7b7d25d032f2bb506d11a84a
Francesco Risso <francesco@frisso.net>
2023-03-16 12:05:25 +0100

    Changed invalid appId capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'politoi.app-y.group4',
+  appId: 'polito.appy.group4',
   appName: 'App-Y',
   webDir: 'build',
   bundledWebRuntime: false
