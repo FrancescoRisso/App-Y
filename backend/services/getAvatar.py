75cfcb0bdaa0004cebadfddc25b16c5a6558bee0
Francesco Risso <francesco@frisso.net>
2023-04-12 17:43:16 +0200

    Improved returns for invalid states

--- backend/services/getAvatar.py
+++ backend/services/getAvatar.py
@@ -4,19 +4,21 @@ from log import log
 
 def getAvatar(db, request):
 	if "userID" not in request.args:
-		return ""
-	
+		return json.dumps(None)
+
 	userId = request.args["userID"]
 
 	try:
 		result = db.run_query("SELECT Is_default FROM avatar WHERE ID=%s", tuple(userId))
 	except Exception as e:
 		log("ERR", e)
-		return ""
+		return "err"
 
 	try:
 		if list(list(result)[0])[0] == 1:
 			return json.dumps({"type": "default"})
-		return json.dumps({"type": "custom", "details": "todo"})
 	except Exception:
-		return ""
+		log("ERR", f"User ID {userId} is not present in the database")
+		json.dumps(None)
+
+	return json.dumps({"type": "custom", "details": "todo"})
