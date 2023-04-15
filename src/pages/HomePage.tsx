d0289c547ad92a156d2838ddf40832f77a13ef8b
Francesco Risso <francesco@frisso.net>
2023-04-15 20:57:09 +0200

    Cleaned homepage (was a test field)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -15,27 +15,11 @@ context:
 */
 
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from "@ionic/react";
-import { useContext, useEffect, useState } from "react";
-import API from "../api";
-import { AppContext } from "../components/AppContext";
 
 export interface HomePageProps {}
 
 const HomePage = () => {
-	const [userInfo, setUserInfo] = useState<string>("");
 
-	const appContext = useContext(AppContext);
-
-	useEffect(() => {
-		const getData = async () => {
-			if (appContext.storage.isOk) {
-				const userID = await appContext.storage.getValue("userID");
-				const pwd = await appContext.storage.getValue("pwd");
-				API.getInfo({ userID, pwd }).then((data) => setUserInfo(JSON.stringify(data)));
-			}
-		};
-		getData();
-	}, [appContext.storage]);
 
 	return (
 		<IonPage>
@@ -45,7 +29,6 @@ const HomePage = () => {
 				</IonToolbar>
 			</IonHeader>
 			<IonContent color="main">
-				<p>{userInfo}</p>
 			</IonContent>
 		</IonPage>
 	);
