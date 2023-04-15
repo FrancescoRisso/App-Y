1e86bcf405e934023e66dda01f04b042ef1f1ced
Francesco Risso <francesco@frisso.net>
2023-04-15 20:57:33 +0200

    Completed registration in the frontend

--- src/components/Register_chain/Common/RegisterFooter.tsx
+++ src/components/Register_chain/Common/RegisterFooter.tsx
@@ -11,10 +11,15 @@ context:
 	
 */
 
-import { IonFooter, IonNavLink, IonButton, IonIcon } from "@ionic/react";
+import { IonFooter, IonNavLink, IonButton, IonIcon, IonAlert } from "@ionic/react";
 import { arrowForwardOutline, enterOutline } from "ionicons/icons";
 
 import Common from "./RegisterCommon";
+import API from "../../../api";
+import { useContext, useRef, useState } from "react";
+import { RegisterContext } from "./RegisterContext";
+import { Link } from "react-router-dom";
+import { AppContext } from "../../AppContext";
 
 export interface RegisterFooterProps {
 	// Whether the "next" button should be enabled
@@ -29,13 +34,46 @@ export interface RegisterFooterProps {
 }
 
 const RegisterFooter = ({ nextPageEnabled, nextClickAction, isLastPage }: RegisterFooterProps) => {
+	const registerContext = useContext(RegisterContext);
+	const appContext = useContext(AppContext);
+
+	const goToMainPage = useRef<HTMLAnchorElement>(null);
+
+	const [usernameInvalid, setUsernameInvalid] = useState<boolean>(false);
+
 	return (
 		<IonFooter className="main-color ion-text-end">
+			<Link to="/home" className="ion-hide" ref={goToMainPage} />
+			<IonAlert
+				isOpen={usernameInvalid}
+				message="L'username scelto è già in uso"
+				buttons={["OK"]}
+				onDidDismiss={() => setUsernameInvalid(false)}
+			></IonAlert>
+
 			{nextPageEnabled ? (
 				isLastPage ? (
-					<IonButton color="main" onClick={async () => {
-						
-					}}>
+					<IonButton
+						color="main"
+						onClick={async () => {
+							const res = await API.register({
+								username: registerContext.username.val,
+								pwd: registerContext.password.val,
+								birthdate: registerContext.birthDate.val.format("YYYY-MM-GG"),
+								name: registerContext.name.val,
+								surname: registerContext.surname.val,
+								sex: registerContext.gender.val === "" ? "other" : registerContext.gender.val
+							});
+
+							if (res?.correct) {
+								await appContext.storage.storeValue("userID", res.userId);
+								await appContext.storage.storeValue("pwd", registerContext.password.val);
+								goToMainPage.current?.click();
+							} else {
+								setUsernameInvalid(true);
+							}
+						}}
+					>
 						<IonIcon slot="end" icon={enterOutline} />
 						Registrati
 					</IonButton>
