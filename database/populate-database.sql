3d7b9417fc9774df995f9ec4cb99ccab058e9d8a
Francesco Risso <francesco@frisso.net>
2023-04-20 15:33:32 +0200

    Added trigger on the db to automatically set a default avatar

--- database/populate-database.sql
+++ database/populate-database.sql
@@ -23,8 +23,7 @@ VALUES
 		TRUE,
 		"2000-02-15",
 		"MarioRossi",
-		"9640b8497989f88328b7f3bd94c6636f00acc35dabab138345a0b9b5088ff2d2"
-		-- ^hash of "HeySoMario"
+		"9640b8497989f88328b7f3bd94c6636f00acc35dabab138345a0b9b5088ff2d2" -- ^hash of "HeySoMario"
 	),
 	(
 		1,
@@ -34,8 +33,7 @@ VALUES
 		false,
 		"1999-05-12",
 		"Luci123",
-		"941d32b55cc70d6ea591dca6bd05bf6da905b40edf19022a263441c08abdf961"
-		-- ^hash of "Luci123"
+		"941d32b55cc70d6ea591dca6bd05bf6da905b40edf19022a263441c08abdf961" -- ^hash of "Luci123"
 	),
 	(
 		2,
@@ -45,16 +43,29 @@ VALUES
 		NULL,
 		"2001-05-01",
 		"Balena456",
-		"011efea0cac926232add733f86e3d9f6ab53c237be9e40f9e6a2e255ba33abc3"
-		-- ^hash of "P4ssw0rd"
+		"011efea0cac926232add733f86e3d9f6ab53c237be9e40f9e6a2e255ba33abc3" -- ^hash of "P4ssw0rd"
 	);
 
-INSERT INTO
-	AVATAR(ID, Is_default)
-VALUES
-	(0, false),
-	(1, TRUE),
-	(2, TRUE);
+UPDATE
+	AVATAR
+SET
+	Is_default = false
+WHERE
+	ID = 0;
+
+UPDATE
+	AVATAR
+SET
+	Is_default = TRUE
+WHERE
+	ID = 1;
+
+UPDATE
+	AVATAR
+SET
+	Is_default = TRUE
+WHERE
+	ID = 2;
 
 INSERT INTO
 	DAILY_SURVEYS(ID, DAY)
