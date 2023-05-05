b2b20e032cf1862482326b3180e664ef9352b460
Francesco Risso <francesco@frisso.net>
2023-05-05 10:24:10 +0200

    Improved speechBubble

--- src/components/General_components/SpeechBubble.tsx
+++ src/components/General_components/SpeechBubble.tsx
@@ -13,8 +13,7 @@ context:
 
 import { IonCard, IonCardContent } from "@ionic/react";
 
-import talkingLabel from "../../images/tmp/talkingLabel.png";
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState, useCallback } from "react";
 
 export interface SpeechBubbleProps {
 	content: string | JSX.Element;
@@ -22,49 +21,28 @@ export interface SpeechBubbleProps {
 
 const SpeechBubble = ({ content }: SpeechBubbleProps) => {
 	const card = useRef<HTMLIonCardElement>(null);
-	const img = useRef<HTMLImageElement>(null);
 
 	const [pos, setPos] = useState<DOMRect>();
 
-	useEffect(() => {
-		// This one sets the position first
-		setTimeout(() => {
-			if (card.current && card.current.getBoundingClientRect() !== pos)
-				setPos(card.current.getBoundingClientRect());
-		}, 10);
-
-		// This should reposition it fast enought so that the user does not notice
-		setTimeout(() => {
-			if (card.current && card.current.getBoundingClientRect() !== pos)
-				setPos(card.current.getBoundingClientRect());
-		}, 30);
-
-		// In extreme cases, this repositions after a while
-		setTimeout(() => {
-			if (card.current && card.current.getBoundingClientRect() !== pos)
-				setPos(card.current.getBoundingClientRect());
-		}, 60);
+	const placeTalkingLabel = useCallback(() => {
+		if (card.current && JSON.stringify(card.current.getBoundingClientRect()) !== JSON.stringify(pos))
+			setPos(card.current.getBoundingClientRect());
 	}, [card, pos]);
+
+	useEffect(placeTalkingLabel, [placeTalkingLabel]);
 
 	return (
 		<>
-			<IonCard ref={card} className="round-speechbubble">
-				<IonCardContent>{content}</IonCardContent>
-			</IonCard>
-			{pos && (
-				<img
-					src={talkingLabel}
-					alt=""
-					width="10%"
-					ref={img}
-					style={{
-						zIndex: 1,
-						position: "fixed",
-						top: pos.y + pos.height - 1,
-						left: pos.x + 0.1 * pos.width
-					}}
-				/>
-			)}
+			<div
+				className="round-speechbubble-wrapper"
+				style={{
+					height: pos ? pos.height + 20 : "100%"
+				}}
+			>
+				<IonCard ref={card} className="round-speechbubble">
+					<IonCardContent>{content}</IonCardContent>
+				</IonCard>
+			</div>
 		</>
 	);
 };
